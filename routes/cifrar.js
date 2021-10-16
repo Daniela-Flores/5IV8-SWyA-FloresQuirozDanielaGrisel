284e507ecf05813d3e73cdbc4ea89e76fd9d2cf6
Daniela-Flores <dfloresq1900@alumno.ipn.mx>
2021-10-16 10:10:45 -0500

    final

---
+++ routes/cifrar.js
@@ -0,0 +1,249 @@
+const express = require('express');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const crypto = require('crypto');
+const bycrypt = require('bcryptjs');
+const aesjs = require('aes-js');
+const fs = require('fs');
+
+console.log(crypto.getCiphers());
+
+router.get('/', (req, res) => {
+  res.render('cifrar.ejs');
+});
+
+router.post(
+  '/',
+  [
+    body('nya', 'Se requiere un nombre').not().isEmpty(),
+    body(
+      'text',
+      'Ingresa una contraseña válida'
+    )
+      .exists()
+      .isLength({
+        min: 16,
+        max: 32,
+      })
+      .matches(/^[a-zA-Z0-9]+(\s*[a-zA-Z0-9]*)*[a-zA-Z0-9]+$/),
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    var valores = req.body;
+    if (!errors.isEmpty()) {
+      console.log(req.body);
+
+      const validaciones = errors.array();
+      return res.render('cifrar.ejs', {
+        validaciones: validaciones,
+        valores: valores,
+      });
+    }
+
+    try {
+      if (valores.type === '192') {
+       
+
+        let secret_message = valores.nya;
+
+        var key = Buffer.from(valores.text);
+        if (key.length === 16 || key.length === 32) {
+          var error = 'No puede cifrar en AES 192';
+
+          console.log(error);
+        } else {
+          if (key.length !== 24) {
+            key = Buffer.from('123456781234567812345678');
+            console.log(key.length);
+            console.log('llave cambiada');
+            var error = 'Contraseña no válida';
+          }
+        }
+
+        var textBytes = aesjs.utils.utf8.toBytes(secret_message);
+
+        var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+
+        var encryptedBytes = aesCtr.encrypt(textBytes);
+
+        var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+
+        fs.writeFile(
+          `./files/encrypted/${encryptedHex}-${encryptedBytes}.txt`,
+          encryptedHex,
+          (err) => {
+            if (err) {
+              return console.error(err);
+            } else {
+              console.log('Archivo Guardado');
+
+              console.log(filename);
+            }
+          }
+        );
+        var filename = `${encryptedHex}-${encryptedBytes}.txt`;
+
+        console.log(encryptedHex);
+        const newValores = {
+          ...valores,
+          encrypted: encryptedHex,
+          err: error,
+          key: key,
+          filename: filename,
+        };
+
+        res.render('cifrar.ejs', { newValores: newValores });
+      }
+      if (valores.type === '256') {
+        let secret_message = valores.nya;
+
+        var key = Buffer.from(valores.text);
+
+        if (key.length === 16 || key.length === 24) {
+          var error = 'No se puede cifrar en AES 256';
+
+          console.log(error);
+        } else {
+          if (key.length !== 32) {
+            key = Buffer.from('12345678123456781234567812345678');
+            console.log(key.length);
+            var error = '';
+          }
+        }
+        var textBytes = aesjs.utils.utf8.toBytes(secret_message);
+
+        var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+
+        var encryptedBytes = aesCtr.encrypt(textBytes);
+
+        var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+
+        fs.writeFile(
+          `./files/encrypted/${encryptedHex}-${encryptedBytes}.txt`,
+          encryptedHex,
+          (err) => {
+            if (err) {
+              return console.error(err);
+            } else {
+              console.log('Archivo Guardado');
+
+              console.log(filename);
+            }
+          }
+        );
+        var filename = `${encryptedHex}-${encryptedBytes}.txt`;
+
+        console.log(encryptedHex);
+        const newValores = {
+          ...valores,
+          encrypted: encryptedHex,
+          err: error,
+          key: key,
+          filename: filename,
+        };
+
+        res.render('cifrar.ejs', { newValores: newValores });
+      }
+      if (valores.type === 'bcrypt') {
+        const salt = await bycrypt.genSalt(10);
+
+        encrypted = await bycrypt.hash(valores.nya, salt);
+
+        fs.writeFile(
+          `./files/encrypted/BCRYPT-${Date.now()}1.txt`,
+          encrypted,
+          (err) => {
+            if (err) {
+              return console.error(err);
+            } else {
+              console.log('Archivo Guardado');
+
+              console.log(filename);
+            }
+          }
+        );
+        var filename = `BCRYPT-${Date.now()}1.txt`;
+
+        const newValores = {
+          ...valores,
+          encrypted: encrypted,
+          filename: filename,
+        };
+
+        res.render('cifrar.ejs', { newValores: newValores });
+      }
+      if (valores.type === '128') {
+        let secret_message = valores.nya;
+
+        var key = Buffer.from(valores.text);
+
+        if (key.length === 24 || key.length === 32) {
+          var error = 'No se puede cifrar en AES 128';
+
+          console.log(error);
+        } else {
+          if (key.length !== 16) {
+            key = Buffer.from('1234567812345678');
+            console.log(key.length);
+            console.log('llave cambiada');
+            var error = 'Contraseña no válida';
+          }
+        }
+        var textBytes = aesjs.utils.utf8.toBytes(secret_message);
+
+        var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+
+        var encryptedBytes = aesCtr.encrypt(textBytes);
+
+        var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+
+        console.log(encryptedHex);
+
+        fs.writeFile(
+          `./files/encrypted/${encryptedHex}-${encryptedBytes}.txt`,
+          encryptedHex,
+          (err) => {
+            if (err) {
+              return console.error(err);
+            } else {
+              console.log('Archivo Guardado');
+
+              console.log(filename);
+            }
+          }
+        );
+        var filename = `${encryptedHex}-${encryptedBytes}.txt`;
+
+        const newValores = {
+          ...valores,
+          encrypted: encryptedHex,
+          err: error,
+          key: key,
+          filename: filename,
+        };
+
+        res.render('cifrar.ejs', { newValores: newValores });
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.render('index.ejs');
+    }
+  }
+);
+
+router.get('/descargar/:id', (req, res) => {
+  res.download(
+    './files/encrypted/' + req.params.id,
+    req.params.id,
+    function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Listo');
+      }
+    }
+  );
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+//5IV8 FLORES QUIROZ DANIELA GRISEL
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', require('./routes/index'));
+app.use('/cifrar', require('./routes/cifrar'));
+app.use('/descifrar', require('./routes/descifrar'));
+
+app.set('view engines', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.listen(PORT, () => console.log(`Server on port ${PORT}`));
